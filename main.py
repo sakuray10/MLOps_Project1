0258cba75cfcdd216bb6bfe167e6b29fabeabe14
Sakura <sakuraya10@gmail.com>
2023-06-03 20:14:17 -0400

    mute detectron

--- main.py
+++ main.py
@@ -26,45 +26,55 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-im = cv2.imread("./input.jpg")
+def detectron(input_img):
 
-'''
-# Window name in which image is displayed
-cv2.imshow("input", im)
+    # im = cv2.imread("./input.jpg")
+    im = cv2.imread(input_img)
 
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing)
-cv2.waitKey(0)
+    '''
+    # Window name in which image is displayed
+    cv2.imshow("input", im)
 
-'''
+    # waits for user to press any key
+    # (this is necessary to avoid Python kernel form crashing)
+    cv2.waitKey(0)
 
-cfg = get_cfg()
-# run without cuda
-cfg.MODEL.DEVICE = "cpu"
+    '''
 
-# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(cfg)
-outputs = predictor(im)
+    cfg = get_cfg()
+    # run without cuda
+    cfg.MODEL.DEVICE = "cpu"
 
-# look at the outputs. See https://detectron2.readthedocs.io/tutorials/models.html#model-output-format for specification
-print(outputs["instances"].pred_classes)
-print(outputs["instances"].pred_boxes)
+    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(im)
 
-# We can use `Visualizer` to draw the predictions on the image.
-v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-cv2.imshow("out", out.get_image()[:, :, ::-1])
+    # look at the outputs. See https://detectron2.readthedocs.io/tutorials/models.html#model-output-format for specification
+    print(outputs["instances"].pred_classes)
+    print(outputs["instances"].pred_boxes)
 
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing) 
-cv2.waitKey(0)
+    # We can use `Visualizer` to draw the predictions on the image.
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
-  
-# closing all open windows
-cv2.destroyAllWindows()
+    output_img = out.get_image()[:, :, ::-1]
+    # cv2.imshow("out", output_img)
 
-print("done")
+    # # waits for user to press any key
+    # # (this is necessary to avoid Python kernel form crashing) 
+    # cv2.waitKey(0)
+
+    
+    # # closing all open windows
+    # cv2.destroyAllWindows()
+
+    # Saving the image
+    cv2.imwrite("out.jpg", output_img)
+
+    return output_img
+
+detectron("input.jpg")
