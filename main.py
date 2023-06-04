450f3e4d922295a67038707a59f554a96281b191
Sakura <sakuraya10@gmail.com>
2023-06-04 09:55:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 # set up detectron2 model
 import torch, detectron2
+import torchvision
 TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
 CUDA_VERSION = torch.__version__.split("+")[-1]
 print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
